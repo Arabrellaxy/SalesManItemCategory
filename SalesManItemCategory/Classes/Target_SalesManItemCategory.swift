2cda89f52bd20ba0d0b3c241914e920f099538a2
xieyan <xiey@sailwish.com>
2018-11-21 09:56:31 +0800

    space change

--- SalesManItemCategory/Classes/Target_SalesManItemCategory.swift
+++ SalesManItemCategory/Classes/Target_SalesManItemCategory.swift
@@ -10,7 +10,7 @@ import UIKit
 class Target_SalesManItemCategory : NSObject {
     @objc  func Action_viewController(_ params:[AnyHashable:Any]?) -> UIViewController{
         let bundle1 = Bundle.init(for: SWViewController.classForCoder())
-        let path = bundle1.path(forResource: "SalesManItemCategory ", ofType: "bundle")!
+        let path = bundle1.path(forResource: "SalesManItemCategory", ofType: "bundle")!
         
         let bundle:Bundle = Bundle.init(path:path)!
         let vc:SWViewController = UIStoryboard.init(name: "Main", bundle: bundle).instantiateViewController(withIdentifier: "category") as! SWViewController
